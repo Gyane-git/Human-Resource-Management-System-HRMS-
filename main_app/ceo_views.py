3c4e0af848457c78ed59888ace6ef53174892fd2
Gyane-git <alpen850@gmail.com>
2025-02-27 19:32:23 +0545

    update in co view

--- main_app/ceo_views.py
+++ main_app/ceo_views.py
@@ -1,5 +1,5 @@
 import json
-import requests
+#import requests
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, JsonResponse
